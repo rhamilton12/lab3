dd79af89381b6fb3931aa2994bca25bb5a952f8b
dohalarbi <larbi.elabidi@gmail.com>
2017-04-01 20:14:55 -0400

    Add files via upload

---
+++ I2c_working_code.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+
+int main( int argc, char **argv )
+{
+        int i;
+        int r;
+        int fd;
+        unsigned char command[2];
+        unsigned char value[4];
+
+        useconds_t delay = 2000;
+
+        char *dev = "/dev/i2c-0";
+        int addr = 0x48;
+
+        printf("TMP102 Test\n");
+
+        fd = open(dev, O_RDWR );
+        if(fd < 0)
+        {
+                perror("Opening i2c device node\n");
+                return 1;
+        }
+
+        r = ioctl(fd, I2C_SLAVE, addr);
+        if(r < 0)
+        {
+                perror("Selecting i2c device\n");
+        }
+
+        while(1)
+        {
+                for(i = 0; i < 4; i++)
+                {
+                       command[0] = 0x40 | ((i + 1) & 0x03); // output enable | read input i
+                       command[1]++;
+                       r = write(fd, &command, 2);
+                        usleep(delay);
+                        // the read is always one step behind the selected input
+                        r = read(fd, &value[i], 1);
+                        if(r != 1)
+                        {
+                                perror("reading i2c device\n");
+                        }
+                        usleep(delay);
+                }
+                
+				printf("Temp = %d degC\n", value[3]);
+			
+        }
+
+        close(fd);
+        return(0);
+}
